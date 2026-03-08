419a8267cbed6664f8ca715a7b99d2d3719a8604
Am Tevi <ultxl01@gmail.com>
2026-03-08 21:37:34 +0300

    tevi

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route("/bot", methods=["POST"])
 def bot():
+    update = request.get_data()
+
     result = subprocess.run(
         ["php", "index.php"],
-        capture_output=True,
-        text=True
+        input=update,
+        capture_output=True
     )
+
     return result.stdout
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
